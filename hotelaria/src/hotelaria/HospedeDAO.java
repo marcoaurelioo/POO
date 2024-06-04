ccb61ebf2599943af222bb65fe897a82c4d9dd29
marcoaurelioo <166968170+marcoaurelioo@users.noreply.github.com>
2024-06-04 18:07:09 -0300

    Update HospedeDAO.java

--- hotelaria/src/hotelaria/HospedeDAO.java
+++ hotelaria/src/hotelaria/HospedeDAO.java
@@ -3,4 +3,5 @@ package hotelaria;
 public interface HospedeDAO {
 	public boolean cadastrar(Hospede hospede);
 	public boolean consultar(Hospede hospede);
+	public boolean editar(Hospede hospede);
 }
