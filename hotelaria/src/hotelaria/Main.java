c9926c8fddc1f6008472d698751c5b3600025029
marcoaurelioo <166968170+marcoaurelioo@users.noreply.github.com>
2024-06-04 20:10:00 -0300

    Testes dos metodos

--- hotelaria/src/hotelaria/Main.java
+++ hotelaria/src/hotelaria/Main.java
@@ -1,25 +1,30 @@
-package hotelaria;
-
-
-public class Main {
-
-    public static void main(String[] args) {
-        HospedeDataDAO dao = new HospedeDataDAO();
-
-        
-        Hospede hospede1 = new Hospede("Joao Silva", "joao@email.com", "12345678900", "Rua das Flores, 123");
-
-        if (dao.cadastrar(hospede1)) {
-            System.out.println("Hospede cadastrado com sucesso!");
-        } else {
-            System.out.println("Erro ao cadastrar hospede.");
-        }
-
-        
-        if (dao.consultar(hospede1)) {
-            System.out.println("Hospede encontrado!");
-        } else {
-            System.out.println("Hospede nao encontrado."); 
-        }
-    }
-}
+	package hotelaria;
+	
+	public class Main {
+	
+		public static void main(String[] args) {
+	        HospedeDataDAO dao = new HospedeDataDAO();
+	
+	        Hospede hospede1 = new Hospede("12345678900", "Joao Silva", "joao@email.com", "Rua das Flores, 123");
+	
+	        if (dao.cadastrar(hospede1)) {
+	            System.out.println("Hospede cadastrado com sucesso!");
+	        } else {
+	            System.out.println("Erro ao cadastrar hospede.");
+	        }
+	        
+	        if (dao.consultar(hospede1)) {
+	            System.out.println("Hospede encontrado!");
+	        } else {
+	            System.out.println("Hospede nao encontrado."); 
+	        }
+	
+	          // Teste do método editar
+	       System.out.println("Editando hóspede 1:");
+	         dao.editar(hospede1);
+	
+	
+	    }
+		
+	
+	}
