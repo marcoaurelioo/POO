f630e1d37dbe4c0e42d67868ca66eaf2efd48825
marcoaurelioo <166968170+marcoaurelioo@users.noreply.github.com>
2024-06-06 19:52:50 -0300

    Update dao.java

--- hotelaria/src/DAO/dao.java
+++ hotelaria/src/DAO/dao.java
@@ -2,6 +2,6 @@ package dao;
 public interface dao<T> {
   public boolean cadastrar(T object);
   public T consultar(T object);
-  public boolean editar(T   hospede);
+  public boolean editar(T hospede);
   public ArrayList<T> listar(T object);
 }
