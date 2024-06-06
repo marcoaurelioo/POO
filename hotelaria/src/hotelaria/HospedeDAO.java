0b937c5469474eb553e60beddb226f39dfffa7bf
marcoaurelioo <166968170+marcoaurelioo@users.noreply.github.com>
2024-06-06 20:19:41 -0300

    Delete hotelaria/src/hotelaria/HospedeDAO.java

--- hotelaria/src/hotelaria/HospedeDAO.java
+++
@@ -1,8 +0,0 @@
-package hotelaria;
-
-public interface HospedeDAO {
-	public boolean cadastrar(Hospede hospede);
-	public boolean consultar(Hospede hospede);
-	public boolean editar(Hospede hospede);
-	public boolean listar(Hospede hospede);
-}
