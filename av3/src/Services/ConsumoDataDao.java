caf462b931106e86ba40ddf216b097f675592f8e
marcoaurelioo <166968170+marcoaurelioo@users.noreply.github.com>
2024-06-11 18:47:13 -0300

    Update ConsumoDataDao.java

--- av3/src/Services/ConsumoDataDao.java
+++ av3/src/Services/ConsumoDataDao.java
@@ -23,6 +23,12 @@ import java.text.SimpleDateFormat;
 public class ConsumoDataDao implements dao<Consumo>{
     private static final String CAMINHO = "C:\\Users\\Marco Aurélio\\Desktop\\POO\\av3\\src\\db\\Consumo.txt";
     
+    public class DaoException extends Exception {
+        public DaoException(String message) {
+            super(message);
+        }
+    }
+    
     private void verificarOuCriarArquivo() {
         File file = new File(CAMINHO);
         if (!file.exists()) {
@@ -31,6 +37,7 @@ public class ConsumoDataDao implements dao<Consumo>{
                 file.createNewFile();
             } catch (IOException e) {
                 e.printStackTrace();
+                throw new DaoException("Erro ao criar ou verificar arquivo."); //  lida com erros relacionados a operações de acesso a dados, como ler ou gravar arquivos.
             }
         }
     }
