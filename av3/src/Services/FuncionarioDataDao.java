ae3e03b85e384a14fbcb81b138cbff60073e41bd
marcoaurelioo <166968170+marcoaurelioo@users.noreply.github.com>
2024-06-11 18:46:28 -0300

    Update FuncionarioDataDao.java

--- av3/src/Services/FuncionarioDataDao.java
+++ av3/src/Services/FuncionarioDataDao.java
@@ -14,6 +14,12 @@ import hotelaria.Funcionario;
 public class FuncionarioDataDao implements dao<Funcionario>{
     private static final String CAMINHO = "C:\\Users\\Marco Aurélio\\Desktop\\POO\\av3\\src\\db\\Funcionario.txt";
     
+    public class DaoException extends Exception {
+        public DaoException(String message) {
+            super(message);
+        }
+    }
+    
     private void verificarOuCriarArquivo() {
         File file = new File(CAMINHO);
         if (!file.exists()) {
@@ -22,6 +28,7 @@ public class FuncionarioDataDao implements dao<Funcionario>{
                 file.createNewFile();
             } catch (IOException e) {
                 e.printStackTrace();
+                throw new DaoException("Erro ao criar ou verificar arquivo."); //  lida com erros relacionados a operações de acesso a dados, como ler ou gravar arquivos.
             }
         }
     }
